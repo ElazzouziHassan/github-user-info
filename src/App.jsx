0e2a416b36b9f281264db1b2fb49298d49ef6fb8
Wizardy <wizardhassan99@gmail.com>
2024-03-18 22:23:48 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import './App.css'
 import UserForm from './components/UserForm'
 import UserInfo from './components/UserInfo'
 
 function App() {
 
+  const [user, setUser] = useState(null);
+  
+  const getUser = (userData) => {
+    setUser(userData);
+  };
+
   return (
     <>
-      <UserForm/>
-      <UserInfo/>
+      <h1>GitHub User Info</h1>
+      <UserForm getUser={getUser} />
+      <UserInfo user={user} />
     </>
   )
 }
