abca711ae17258279033edc6cc164449f8263d77
Wizardy <wizardhassan99@gmail.com>
2024-03-21 22:49:55 +0000

    commit

--- src/components/info/UserInfo.jsx
+++ src/components/info/UserInfo.jsx
@@ -4,14 +4,34 @@ import "./user-info.scss";
 export default function UserInfo({user}) {
   return (
     <div className="user-info">
-      <h2>User Info</h2>
       {user && (
         <div>
-          <p>Name: {user.name}</p>
-          <p>Username: {user.login}</p>
-          <p>Followers: {user.followers}</p>
-          <p>Following: {user.following}</p>
-          <p>Public Repos: {user.public_repos}</p>
+          <div className="user-card">
+            <div className="avatar">
+              <img src={user.avatar_url} alt="avatr" />
+            </div>
+            <div className="bio">
+              <h2>{user.name}</h2>
+              <h2>{user.location}</h2>
+              <p>
+                {user.bio}
+              </p>
+            </div>
+            <div className="stats">
+              <div className="repos">
+                <span>Repositories</span>
+                <span>{user.public_repos}</span>
+              </div>
+              <div className="followers">
+                <span>Followers</span>
+                <span>{user.followers}</span>
+              </div>
+              <div className="following">
+                <span>Following</span>
+                <span>{user.following}</span>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
