3d4562b1736ec7c6e215e057370fb121c9f73aef
Wizardy <wizardhassan99@gmail.com>
2024-03-21 21:56:12 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import './App.css'
-import UserForm from './components/form/UserForm'
-import UserInfo from './components/info/UserInfo'
+import UserForm from './components/form/UserForm';
+import UserInfo from './components/info/UserInfo';
+import './App.scss';
 
 function App() {
 
@@ -14,11 +14,13 @@ function App() {
   return (
     <div className="app">
       <h1>GitHub User Info</h1>
-      <div className="form">
-        <UserForm getUser={getUser} />
-      </div>
-      <div className="info">
-        <UserInfo user={user} />
+      <div className="main">
+        <div className="form">
+          <UserForm getUser={getUser} />
+        </div>
+        <div className="info">
+          <UserInfo user={user} />
+        </div>
       </div>
     </div>
   )
